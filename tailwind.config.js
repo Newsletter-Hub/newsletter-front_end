1f65d7a401620cf7dc47d20078186662e5f20d31
Vadym Hlushko <ver2ex@MacBook-Air-Vadym.local>
2023-04-14 21:30:40 +0300

    add radix ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
+    colors: {
+      primary: '#01AAED',
+      black: '#000000',
+      'light-grey': '#6F6C90',
+      'dark-grey': '#404040',
+      grey: '#5A616D',
+      'gull-grey': '#A3A9AC',
+      yellow: '#F7B500',
+      white: '#FFFFFF',
+      facebookBlue: '#3B5998',
+      twitterBlue: '#4099FF',
+      googleBlack: '#25282B',
+      mercury: '#E1E1E1',
+    },
   },
   plugins: [],
 };
