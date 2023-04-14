088b48702fa8a81784c0a47d26000adb01dcb449
Vadym Hlushko <ver2ex@MacBook-Air-Vadym.local>
2023-04-14 21:11:43 +0300

    delete nextui

---
+++ config/theme.ts
@@ -0,0 +1,20 @@
+const theme = createTheme({
+  theme: {
+    colors: {
+      primary: '#01AAED',
+      black: '#000000',
+      lightGrey: '#6F6C90',
+      darkGrey: '#404040',
+      grey: '#5A616D',
+      gullGrey: '#A3A9AC',
+      yellow: '#F7B500',
+      white: '#FFFFFF',
+      facebookBlue: '#3B5998',
+      twitterBlue: '#4099FF',
+      googleBlack: '#25282B',
+      mercury: '#E1E1E1',
+    },
+  },
+});
+
+export default theme;
