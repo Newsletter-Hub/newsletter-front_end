707c9d227dd8360cb4f79722c4637387d7e7b6a0
Vadym Hlushko <ver2ex@MacBook-Air-Vadym.local>
2023-04-14 17:30:19 +0300

    add ky config and install nextui, react-query

--- config/ky.ts
+++ config/ky.ts
@@ -1,7 +1,9 @@
-import ky from 'ky'
+import ky from 'ky';
 
-const baseUrl = 'http://apis.somecourseurl.com/api/'
+const baseUrl = 'http://apis.somecourseurl.com/api/';
 
-export const api = ky.create({ prefixUrl: baseUrl })
+export const publicApi = ky.create({ prefixUrl: baseUrl });
 
-export default api
+const api = ky.create({ prefixUrl: baseUrl });
+
+export default api;
