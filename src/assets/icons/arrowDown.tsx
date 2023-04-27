a96381e1d2c4b16cc4aa66bbb53e751752c5a030
ver2ex1 <vadym.hl@darly.solutions>
2023-04-27 11:30:21 +0300

    create sign up

---
+++ src/assets/icons/arrowDown.tsx
@@ -0,0 +1,21 @@
+const ArrowDownIcon = () => {
+  return (
+    <svg
+      width="29"
+      height="29"
+      viewBox="0 0 29 29"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.6709 11.2891L14.653 18.2711L21.635 11.2891"
+        stroke="#A0A4A8"
+        strokeWidth="2.32735"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ArrowDownIcon;
