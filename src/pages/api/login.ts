27006e84b33c407b0dbd48d11473f62fbf957035
ver2ex1 <vadym.hl@darly.solutions>
2023-04-27 12:32:03 +0300

    add login logic

---
+++ src/pages/api/login.ts
@@ -0,0 +1,13 @@
+import { publicApi } from '@/config/ky';
+
+interface User {
+  email: string;
+  password: string;
+}
+
+export const login = async ({ email, password }: User) => {
+  const response = await publicApi
+    .post('/login', { json: { email, password } })
+    .json();
+  return response;
+};
