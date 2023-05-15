f2827d2af5616dcf4beb27f19cb939dd000612a1
ver2ex1 <vadym.hl@darly.solutions>
2023-05-15 16:18:55 +0300

    add search filter button and sort

---
+++ src/assets/icons/check.tsx
@@ -0,0 +1,21 @@
+const CheckIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M1.0002 8.23249L5.88388 13.1161L15 4"
+        stroke="#253646"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default CheckIcon;
