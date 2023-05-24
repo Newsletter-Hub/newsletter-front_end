f93e28b8b5ddf63b1654ede168d288817c8e6345
= <=>
2023-05-24 12:11:09 +0300

    add action

--- src/actions/newsletters/index.ts
+++ src/actions/newsletters/index.ts
@@ -39,6 +39,11 @@ export interface GetNewsletterListProps {
   search?: string;
 }
 
+interface FollowPayload {
+  entityId: number;
+  entityType: 'Newsletter' | 'User';
+}
+
 export const newsletterVerifyOwnership = async ({
   link,
 }: NewsletterLink): Promise<NewsletterLinkResponse | undefined> => {
@@ -168,5 +173,18 @@ export const getNewslettersList = async ({
     return {
       error: 'Failed to get newsletter',
     };
+  }
+};
+
+export const follow = async ({ entityId, entityType }: FollowPayload) => {
+  try {
+    const response = await api.post('subscriptions', {
+      json: { entityId, entityType },
+    });
+    if (response.ok) {
+      return await response.json();
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
