d8709e894a2ce17e4f1ab5ecc27487650d2fd08e
= <=>
2023-06-01 00:59:17 +0300

    add logic for notification

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
 import { Notification as NotificationType } from '@/types/user';
+import { format, parseISO } from 'date-fns';
 
 import Avatar from './Avatar';
 import StarRating from './StarRating';
@@ -32,105 +33,114 @@ const Notification = ({
     );
   return (
     <div
-      className={`flex py-4 items-center gap-8 font-inter ${
+      className={`flex justify-between items-center py-4 font-inter ${
         !isLast && 'border-b border-b-light-grey'
       }`}
     >
-      <Avatar
-        src={notification.notificationAuthor?.avatar}
-        width={48}
-        height={48}
-        alt="Notification author"
-        username={notification.notificationAuthor?.username}
-      />
-      {notification.notificationType === 'newNewsletter' &&
-        notification.entity && (
-          <div className="text-base text-dark-grey flex items-center gap-4">
-            <div className="text-dark-blue font-semibold text-base">
-              {userState}
+      <div className="flex items-center gap-8">
+        <Avatar
+          src={notification.notificationAuthor?.avatar}
+          width={48}
+          height={48}
+          alt="Notification author"
+          username={notification.notificationAuthor?.username}
+        />
+        {notification.notificationType === 'newNewsletter' &&
+          notification.entity && (
+            <div className="text-base text-dark-grey flex items-center gap-4">
+              <div className="text-dark-blue font-semibold text-base">
+                {userState}
+              </div>
+              <span>created the &nbsp;</span>
+              <Link
+                href={`/newsletters/${notification.entity?.id}`}
+                className="text-primary font-semibold block max-w-[450px] whitespace-nowrap overflow-hidden text-ellipsis"
+              >
+                {'title' in notification.entity && notification.entity.title}
+              </Link>
             </div>
-            <span>created the &nbsp;</span>
-            <Link
-              href={`/newsletters/${notification.entity?.id}`}
-              className="text-primary font-semibold block max-w-[550px] whitespace-nowrap overflow-hidden text-ellipsis"
-            >
-              {'title' in notification.entity && notification.entity.title}
-            </Link>
-          </div>
-        )}
-      {notification.notificationType === 'newReview' && notification.entity && (
-        <div>
-          <div className="text-base text-dark-grey flex items-center gap-4 mb-[6px]">
-            <div className="text-dark-blue font-semibold text-base">
-              {userState}
+          )}
+        {notification.notificationType === 'newReview' &&
+          notification.entity && (
+            <div>
+              <div className="text-base text-dark-grey flex items-center gap-4 mb-[6px]">
+                <div className="text-dark-blue font-semibold text-base">
+                  {userState}
+                </div>
+                <span>
+                  rated{' '}
+                  {notification.notificationAuthorId !==
+                  notification.notificationRecipientId
+                    ? 'your'
+                    : notification.notificationAuthor?.username}{' '}
+                  &nbsp;
+                </span>
+                <Link
+                  href={`/newsletters/${notification.entity?.id}`}
+                  className="text-primary font-semibold block max-w-[350px] whitespace-nowrap overflow-hidden text-ellipsis"
+                >
+                  {'newsletter' in notification.entity &&
+                    notification.entity.newsletter.title}
+                </Link>
+                {'rating' in notification.entity && (
+                  <StarRating
+                    value={notification.entity.rating}
+                    readonly
+                    customStyles="ml-4"
+                  />
+                )}
+              </div>
+              <p className="text-dark-blue text-base">
+                “
+                {'comment' in notification.entity &&
+                  notification.entity.comment}
+                “
+              </p>
             </div>
-            <span>
-              rated{' '}
-              {notification.notificationAuthorId !==
-              notification.notificationRecipientId
-                ? 'your'
-                : notification.notificationAuthor?.username}{' '}
-              &nbsp;
-            </span>
-            <Link
-              href={`/newsletters/${notification.entity?.id}`}
-              className="text-primary font-semibold block max-w-[550px] whitespace-nowrap overflow-hidden text-ellipsis"
-            >
-              {'newsletter' in notification.entity &&
-                notification.entity.newsletter.title}
-            </Link>
-            {'rating' in notification.entity && (
-              <StarRating
-                value={notification.entity.rating}
-                readonly
-                customStyles="ml-4"
-              />
-            )}
-          </div>
-          <p className="text-dark-blue text-base">
-            “{'comment' in notification.entity && notification.entity.comment}“
-          </p>
-        </div>
-      )}
-      {notification.notificationType === 'subscriptionToUser' &&
-        notification.entity && (
-          <div className="flex gap-4 items-center">
-            <div className="text-dark-blue font-semibold text-base">
-              {userState}
+          )}
+        {notification.notificationType === 'subscriptionToUser' &&
+          notification.entity && (
+            <div className="flex gap-4 items-center">
+              <div className="text-dark-blue font-semibold text-base">
+                {userState}
+              </div>
+              <span className="text-base text-dark-grey">
+                started following you
+              </span>
+              <Link href={`/users/${notification.notificationAuthorId}`}>
+                <Button
+                  rounded="xl"
+                  label={
+                    <p className="flex items-center gap-2">
+                      <PlusIcon />
+                      <span className="text-base">Follow back</span>
+                    </p>
+                  }
+                />
+              </Link>
             </div>
-            <span className="text-base text-dark-grey">
-              started following you
-            </span>
-            <Link href={`/users/${notification.notificationAuthorId}`}>
-              <Button
-                rounded="xl"
-                label={
-                  <p className="flex items-center gap-2">
-                    <PlusIcon />
-                    <span className="text-base">Follow back</span>
-                  </p>
-                }
-              />
-            </Link>
-          </div>
-        )}
-      {notification.notificationType === 'subscriptionToNewsletter' &&
-        notification.entity && (
-          <div className="flex gap-4 items-center">
-            <div className="text-dark-blue font-semibold text-base">
-              {userState}
+          )}
+        {notification.notificationType === 'subscriptionToNewsletter' &&
+          notification.entity && (
+            <div className="flex gap-4 items-center">
+              <div className="text-dark-blue font-semibold text-base">
+                {userState}
+              </div>
+              <span className="text-base text-dark-grey">
+                started following {isProfile && 'your'}
+              </span>
+              <Link
+                href={`/newsletters/${notification.entity?.id}`}
+                className="text-primary font-semibold block max-w-[450px] whitespace-nowrap overflow-hidden text-ellipsis"
+              >
+                {'title' in notification.entity && notification.entity.title}
+              </Link>
             </div>
-            <span className="text-base text-dark-grey">
-              started following {isProfile && 'your'}
-            </span>
-            <Link
-              href={`/newsletters/${notification.entity?.id}`}
-              className="text-primary font-semibold block max-w-[550px] whitespace-nowrap overflow-hidden text-ellipsis"
-            >
-              {'title' in notification.entity && notification.entity.title}
-            </Link>
-          </div>
-        )}
+          )}
+      </div>
+      <span className="text-sm text-grey">
+        {format(parseISO(notification.createdAt as string), 'dd.MM.yyyy')}
+      </span>
     </div>
   );
 };
