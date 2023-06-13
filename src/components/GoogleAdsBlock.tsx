9134f919387999cd578f0b0c68560b7d3562cea1
= <=>
2023-06-13 16:47:31 +0300

    fix google ads

--- src/components/GoogleAdsBlock.tsx
+++ src/components/GoogleAdsBlock.tsx
@@ -3,11 +3,16 @@ import Script from 'next/script';
 const GoogleAds = () => {
   return (
     <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8804004591913052"
+        crossOrigin="anonymous"
+      ></Script>
       <ins
         className="adsbygoogle"
         style={{ display: 'block' }}
         data-ad-client="ca-pub-8804004591913052"
-        data-ad-slot="2040931016"
+        data-ad-slot="5662242139"
         data-ad-format="auto"
         data-full-width-responsive="true"
       ></ins>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8804004591913052"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
