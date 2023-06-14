e71b4780a7bb4d71510b35b741dfe679a5b88c92
= <=>
2023-06-14 14:29:37 +0300

    remove ads and add padding bottom

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -20,7 +20,7 @@ const Layout = ({ children, isFooter }: LayoutProps) => {
         <title>Newsletter Hub</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className={`${alegreya.className} overflow-x-hidden`}>
+      <div className={`${alegreya.className} overflow-x-hidden pb-5`}>
         <Header />
         <div className="mx-auto">{children}</div>
         {/* {isFooter && <Footer />} */}
