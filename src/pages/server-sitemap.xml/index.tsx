5c6e619268d8dfdb91d56a63bfb0960181b8c6a5
Jacky Li <jackyli@Jackys-MacBook-Pro.local>
2024-01-23 21:11:10 -0800

    test only adding 25 newsletters to sitemap

--- src/pages/server-sitemap.xml/index.tsx
+++ src/pages/server-sitemap.xml/index.tsx
@@ -7,7 +7,7 @@ export const getServerSideProps: GetServerSideProps = async ctx => {
   // Note if we have over 5000 newsletters, we should create an index server sitemap
   const newsletterListResponse = await getNewslettersList({
     page: 1,
-    pageSize: 50,
+    pageSize: 25,
     order: 'date',
     orderDirection: 'ASC',
   });
