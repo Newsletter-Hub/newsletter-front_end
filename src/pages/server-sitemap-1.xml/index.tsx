bd9ba6a3f5bc600ce8c77501fac1925e5a4125b8
Jacky Li <jackyli@Jackys-MacBook-Pro.local>
2024-01-23 22:19:39 -0800

    use 45

--- src/pages/server-sitemap-1.xml/index.tsx
+++ src/pages/server-sitemap-1.xml/index.tsx
@@ -5,7 +5,7 @@ import { getNewslettersList } from '@/actions/newsletters';
 export const getServerSideProps: GetServerSideProps = async ctx => {
   const newsletterListResponse = await getNewslettersList({
     page: 1,
-    pageSize: 50,
+    pageSize: 45,
     order: 'date',
     orderDirection: 'ASC',
   });
