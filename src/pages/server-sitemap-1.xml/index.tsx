5d45ce1604efaa6e4111d44ebcfb018bf1e8786a
Jacky Li <jackyli@Jackys-MacBook-Pro.local>
2024-01-23 22:52:04 -0800

    hack to add all newsletters to sitemap

--- src/pages/server-sitemap-1.xml/index.tsx
+++ src/pages/server-sitemap-1.xml/index.tsx
@@ -4,19 +4,21 @@ import { getNewslettersList } from '@/actions/newsletters';
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
   const newsletterListResponse = await getNewslettersList({
-    page: 1,
-    pageSize: 25,
     order: 'date',
-    orderDirection: 'ASC',
+    orderDirection: 'DESC',
   });
   const newsletterListData = newsletterListResponse.newslettersListData;
   const newsletters = newsletterListData?.newsletters || [];
+  const latestNewsletterId = newsletters[0]?.id || 0;
   const defaultLastmod = new Date().toISOString();
+  const fields: ISitemapField[] = [];
 
-  const fields: ISitemapField[] = newsletters.map(newsletter => ({
-    loc: `${process.env.NEXT_PUBLIC_BASE_URL}/newsletters/${newsletter.id}`,
-    lastmod: defaultLastmod,
-  }));
+  for (let i = 1; i <= latestNewsletterId; i++) {
+    fields.push({
+      loc: `${process.env.NEXT_PUBLIC_BASE_URL}/newsletters/${i}`,
+      lastmod: defaultLastmod,
+    });
+  }
 
   return getServerSideSitemapLegacy(ctx, fields);
 };
